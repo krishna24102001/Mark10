b3bd84882f0cf05f3f443898b7723df45a1dde54
krishna24102001 <krishnargite1@gmail.com>
2022-10-31 19:25:48 +0530

    Checkpoint
    Structure done

---
+++ index.js
@@ -0,0 +1,8 @@
+const billAmount = document.querySelector("#billamt");
+const cashGiven = document.querySelector("#givencash");
+var calculateButton = document.querySelector(".calculatevalue");
+
+
+calculateButton.addEventListener("click", () => console.log("clicked"));
+
+// console.log(cashGiven.value);
