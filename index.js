30fbcb8b3e991d6fd343f79f6facccf2be05c0eb
krishna24102001 <krishnargite1@gmail.com>
2022-11-01 15:38:40 +0530

    made it

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
-const billAmount = document.querySelector("#billamt");
-const cashGiven = document.querySelector("#givencash");
-var calculateButton = document.querySelector(".calculatevalue");
+const next = document.getElementById('next-btn');
+const checkout = document.getElementById('checkout')
+const table = document.getElementsByTagName('table')[0];
+const checkoutBtn = document.querySelector('#checkout-btn')
+const billAmount = document.querySelector('.bill-Amount')
+const checkoutPay = document.querySelector('.checkout-Amount')
+let td = document.querySelectorAll('.noOfNotes')
 
+const notes =[2000,500,100,20,10,5,1];
+let returnNotes =[]
+next.addEventListener('click', function () {
+    if(billAmount.value>0){
+        checkout.classList.replace('checkout','checkout-show');
+    }
+})
 
-calculateButton.addEventListener("click", () => console.log("clicked"));
+checkoutBtn.addEventListener('click', function(){
+   table.style.visibility ='visible'
+   const bill =  billAmount.value;
+   const checkoutAmount = checkoutPay.value;
+   if(bill>0){
+       if(checkoutAmount>=bill){
+            let changeCash = checkoutAmount - bill;
+            calculateChange(changeCash)
+        }else{
+            alert('Unsufficient cash, please enter adequate cash amount!')
+        }
+    }else{
+        alert("Invalid Bill Amount")
+    }
+})
 
-// console.log(cashGiven.value);
+const calculateChange =(amountToBeReturned)=> {
+    for (let i = 0; i < notes.length; i++) {
+  
+      const numberOfNotes = Math.trunc(amountToBeReturned / notes[i]);
+      amountToBeReturned = amountToBeReturned % notes[i];
+
+      td[i].innerText = numberOfNotes;
+    }
+  }
